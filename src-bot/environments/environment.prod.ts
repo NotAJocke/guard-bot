cd6ce6c166128ebc2805b7ea70c8e4e38e7ea6d7
JockeRider199 <jockerider199@gmail.com>
2023-01-10 10:12:14 +0100

    prod settings

--- src-bot/environments/environment.prod.ts
+++ src-bot/environments/environment.prod.ts
@@ -1,15 +1,16 @@
+import { GatewayIntentBits } from "discord.js";
 import Config from "../models/config";
 
 const config: Config = {
-	intents: [],
+	intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds],
 	loggingChannels: {
 		ban: {
 			name: "",
 			id: "",
 		},
 	},
-	communityRoleVerified: "",
-	guildId: "",
+	communityRoleVerified: "1023342162882068480",
+	guildId: "1022906024816017510",
 };
 
 export default config;
--- src-bot/events/ready.ts
+++ src-bot/events/ready.ts
@@ -5,6 +5,9 @@ import { checkUnbans } from "../utils/checkUnbans";
 export const readyEvent: Event = {
 	async exec(client: Client) {
 		console.log(`Logged in as ${client.user?.tag}!`);
+		if (process.env.PRODUCTION == "TRUE") {
+			console.log("Prod mode")
+		}
 
 		checkUnbans(client);
 	},
