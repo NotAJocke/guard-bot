418a189aad15aee963e5a6195afdd8c81677138f
JockeRider199 <jockerider199@gmail.com>
2023-01-11 16:25:13 +0100

    feat: ping cmd

--- src-bot/commands/test/ping.ts
+++ src-bot/commands/test/ping.ts
@@ -1,42 +1,35 @@
 import {
 	EmbedBuilder,
 	ChatInputCommandInteraction,
-	SlashCommandBuilder,
-	ActionRowBuilder,
-	ButtonBuilder,
-	ButtonStyle,
+	SlashCommandBuilder
 } from "discord.js";
 import { SlashCommand } from "../../models/slash-commands";
 
 const slashCommand: SlashCommand = {
-	data: new SlashCommandBuilder().setName("ping").setDescription("Test"),
+	data: new SlashCommandBuilder()
+		.setName("ping")
+		.setDescription("Developer data"),
 
 	async exec(interaction: ChatInputCommandInteraction) {
-		let embed = new EmbedBuilder({
-			title: "Test",
-			fields: [{ name: "Reply", value: "Pong !" }],
-		});
 
-		let row = new ActionRowBuilder<ButtonBuilder>().addComponents(
-			new ButtonBuilder()
-				.setCustomId("ping_btn")
-				.setLabel("Try")
-				.setStyle(ButtonStyle.Primary)
-		);
+		let embed = new EmbedBuilder()
+			.setAuthor({
+				name: interaction.client.user.username,
+				iconURL: interaction.client.user.displayAvatarURL(),
+			})
+			.setTitle("Developer data")
+			.addFields([
+				{ name: "Latency", value: `${Date.now() - interaction.createdTimestamp}ms` },
+				{ name: "API Ping", value: `${Math.round(interaction.client.ws.ping)}ms` },
+				{ name: "Uptime", value: `${Math.floor(interaction.client.uptime / (60 * 1000 * 60 * 24))} days` },
+				{ name: "Memory usage", value: `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}mb` }
+			])
 
-		interaction.reply({ embeds: [embed], components: [row], ephemeral: true });
-	},
-
-	async execButtons(interaction, buttonId) {
-		switch (buttonId) {
-			case "btn":
-				interaction.reply({ content: "Hello World 2", ephemeral: true });
-				break;
-		}
+		interaction.reply({ embeds: [embed], ephemeral: true });
 	},
 
 	settings: {
-		enabled: false,
+		enabled: true,
 	},
 };
 
