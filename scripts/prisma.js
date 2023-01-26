7ec7b50f47c5727c09674fd6fa7d1d817a731ec9
JockeRider199 <jockerider199@gmail.com>
2023-01-26 14:18:48 +0100

    fix(db): select prod url with docker

---
+++ scripts/prisma.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const { exec } = require("child_process")
+
+let fileData = fs.readFileSync("./dist/prisma/schema.prisma", "utf-8")
+let data = fileData.split("\n");
+
+data.map((l) => {
+  if (l.trim().startsWith("url")) {
+    let index = data.indexOf(l);
+    if(l.split('"')[1] == "PROD_DATABASE_URL") {
+      data[index] = 'url = env("DATABASE_URL")';
+    } else {
+      data[index] = 'url = env("PROD_DATABASE_URL")';
+    }
+  }
+});
+
+fs.writeFileSync("./dist/prisma/schema.prisma", data.join("\n"), { encoding: "utf-8" });
+exec("pnpx prisma format")
