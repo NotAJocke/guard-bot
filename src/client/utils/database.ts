26f35d558d9017ba06b7a2332870fe9bad45c800
JockeRider199 <jockerider199@gmail.com>
2023-03-14 08:57:53 +0100

    build: bump prisma deps

--- src/client/utils/database.ts
+++ src/client/utils/database.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Member, Ban } from "../../node_modules/.prisma/client";
+import { PrismaClient, Member, Ban } from "../../../node_modules/.prisma/client";
 
 const prisma = new PrismaClient();
 
